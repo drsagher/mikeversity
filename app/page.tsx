ba06d3314b164c79977b0d85882d486853a92b33
Abubakar Siddique <dr.sagher@gmail.com>
2024-06-15 22:16:48 -0700

    Home page dn Global CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
-import NavComponent from "@/app/compo/nav";
 import HeroComponent from "@/app/compo/hero";
-import FooterComponent from "@/app/compo/footer";
 import TopCoursesComponent from "@/app/compo/topcourses";
-import SummaryComponent from "@/app/compo/summary";
 import SoftSkillsComponent from "@/app/compo/softskills";
+import TrainingDomainsComponent from "@/app/compo/trainingdomains";
+import MissionComponent from "@/app/compo/mission";
+import CommunityComponent from "@/app/compo/community";
+import ExpertiseComponent from "@/app/compo/expertise";
+import TestimonialsComponent from "@/app/compo/testemonials";
+import ImpactsComponent from "@/app/compo/impacts";
+import VisionComponent from "@/app/compo/vision";
 
 export default function Home() {
   return (
     <main >
-        <NavComponent/>
+        <MissionComponent/>
+        <VisionComponent/>
         <HeroComponent />
         <TopCoursesComponent/>
-        <SummaryComponent/>
+        <TrainingDomainsComponent/>
         <SoftSkillsComponent/>
-        <FooterComponent/>
-
+        <ExpertiseComponent />
+        <ImpactsComponent/>
+        <TestimonialsComponent />
+        <CommunityComponent/>
     </main>
   );
 }
