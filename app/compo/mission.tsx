3a754964cfad26de5f54c5c8b1d207d436735097
Abubakar Siddique <dr.sagher@gmail.com>
2024-11-07 07:27:57 -0800

    Bug fix

--- app/compo/mission.tsx
+++ app/compo/mission.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function MissionComponent(){
     return (
-        <div className={"w-full h-48 sm:h-56 md:h-64 lg:h-80 xl:h-96 2xl:h-[600px] overflow-hidden"}>
-            <iframe className={'w-full h-full'}
-                 src="https://www.youtube.com/embed/_siqlqRYJp8?si=QsLtdwloC5C1qQfO&autoplay=1&mute=1&controls=0&modestbranding=0&rel=0&playsinline=1&enablejsapi=1&showinfo=0"
+        <div className={"w-full aspect-video sm:h-56 md:h-64 lg:min-h-screen"}>
+            <iframe className={'w-full h-full '}
+                 src="https://www.youtube.com/embed/_siqlqRYJp8?si=QsLtdwloC5C1qQfO&autoplay=0&mute=0&controls=0&modestbranding=0&rel=0&playsinline=1&enablejsapi=1&showinfo=0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
 
